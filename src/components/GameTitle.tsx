77411ec14c37e8d483245fc246e932e20db50222
Jozef Chovan <jozef.chovan@gmail.com>
2023-09-20 08:16:30 +0200

    Add Formik

--- src/components/GameTitle.tsx
+++ src/components/GameTitle.tsx
@@ -1,10 +1,19 @@
 import { useState } from 'react';
+import { useFormik } from 'formik';
+import classNames from 'classnames';
+import * as yup from 'yup';
 import { PencilIcon } from './PencilIcon';
 import { CloseIcon } from './CloseIcon';
 import { ModalDialog } from './ModalDialog';
 
 export function GameTitle() {
   const [show, setShow] = useState(false);
+
+  const formik = useFormik({
+    initialValues: { title: '' },
+    validationSchema: yup.object({ title: yup.string().max(24).required() }),
+    onSubmit: (values) => console.log(values)
+  });
 
   return (
     <div className="relative m-8 rounded-lg border-2 border-orange-600 bg-orange-200 text-orange-900">
@@ -18,16 +27,25 @@ export function GameTitle() {
           </div>
         </div>
         <div className="p-3">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis nostrum delectus modi placeat. Mollitia
-          obcaecati sed, pariatur optio nisi, facere sint, hic sequi magni reprehenderit enim? Facilis ipsam quidem
-          odio.
+          <form id="form" onSubmit={formik.handleSubmit}>
+            <input
+              id="title"
+              name="title"
+              type="text"
+              placeholder="Title"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.title}
+              className={classNames({ 'bg-red-200': formik.touched.title && formik.errors.title })}
+            />
+          </form>
         </div>
         <div className="border-t p-3">
           <div className="flex justify-end gap-1">
             <button className="rounded-lg bg-orange-300 px-6 py-1" onClick={() => setShow(false)}>
               Close
             </button>
-            <button className="rounded-lg bg-orange-900 px-6 py-1 text-white" onClick={() => setShow(false)}>
+            <button type="submit" form="form" className="rounded-lg bg-orange-900 px-6 py-1 text-white">
               Save
             </button>
           </div>
