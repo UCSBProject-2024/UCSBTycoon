088c464289079d9a752043ec732c3a6c6e01e75c
Tyler Canepa <tmcanepa@ucsb.com>
2024-05-08 16:38:44 -0700

    added costs

--- src/components/GameCookie.tsx
+++ src/components/GameCookie.tsx
@@ -18,12 +18,13 @@ export function GameCookie() {
   const [isAnimationPlaying, setIsAnimationPlaying] = useState(false);
 
   function handleClick() {
-    if (!gameDispatch) return;
     gameDispatch({ type: 'update', subtype: 'cash' });
     if (!isAnimationPlaying) {
       setIsAnimationVisible(true);
     }
   }
+
+  
 
   function onAnimationStart() {
     setIsAnimationPlaying(true);
@@ -43,21 +44,20 @@ export function GameCookie() {
     const interval = setInterval(() => {
       const game = JSON.parse(localStorage.getItem(LOCAL_STORAGE_COOKIE_CLICKER_GAME_KEY)) as Game;
       setData(game?.knowledge); // Update data every second
-    }, 1000);
-  
-    return () => clearInterval(interval); // Clean up on unmount
+    }, 100);
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div style={{ position: 'relative' }}>
-      <div className="my-8 flex flex-col items-center" style={{ position: 'absolute', top: '30px', zIndex: 2 }}>
+      <div className="my-8 flex flex-col items-center" style={{ position: 'absolute', top: '40px', zIndex: 2 }}>
         <CakeIcon onClick={handleClick} />
         <p className="text-xs text-gray-600" style={{ position: 'absolute', top: '100%' }}>Click on the Gaucho!</p>
       </div>
-      <div className="my-8" style={{ position: 'absolute', left: '45%' }}>
-        <h2 className="text-center text-3xl font-semibold text-orange-900 md:text-5xl">{game?.cash}</h2>
+      <div className="my-8" style={{ position: 'absolute', left: '35%' }}>
+        <h2 className="text-center text-3xl font-semibold text-green-500 md:text-5xl"style={{ fontSize: '1.5rem' }}>{game?.cash}</h2>
         <div className="my-8" style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)' }}>
-          <h2 className="text-center text-3xl font-semibold text-blue-900 md:text-5xl">{data}</h2>
+          <h2 className="text-center text-3xl font-semibold text-blue-900 md:text-5xl" style={{ fontSize: '1.5rem' }}>{data}</h2>
         </div>
       </div>
       {isAnimationVisible && (
--- src/components/MonetaryBuildings.tsx
+++ src/components/MonetaryBuildings.tsx
@@ -4,6 +4,8 @@ import { useGameDispatch } from '../hooks/useGameDispatch';
 import { useGame } from '../hooks/useGame';
 
 interface BuildingProps {
+  cost: number;
+  knowledge: number;
   x: number;
   y: number;
   hasBeenBought?: boolean;
@@ -43,108 +45,108 @@ const MonetaryBuildings: React.FC = () => {
   }, [clickedButtons]); // Update useEffect dependencies
 
   const handleClick = (buildingName: string) => () => {
-    if (!clickedButtons.has(buildingName)) {
+    if (!clickedButtons.has(buildingName)|| clickedButtons.has(buildingName)) {
       console.log('Building clicked:', buildingName);
 
       // Mark the building as bought in local storage
-      if (buildingName === 'The Club & Guest House' && game?.cash >= 100) {
+      if (buildingName === 'The Club & Guest House' && game?.cash >= 100 && game?.knowledge >= 50) {
         localStorage.setItem(buildingName, 'true');
         gameDispatch({ type: 'update', subtype: 'cashMult' }); //sends building name to backend
         console.log('increment cash');
         gameDispatch({ type: 'update', subtype: 'setCash', cash: 100});
       }
-      if (buildingName === 'University Center(UCEN)' && game?.cash >= 500) {
+      if (buildingName === 'University Center(UCEN)' && game?.cash >= 500 && game?.knowledge >= 250) {
         localStorage.setItem(buildingName, 'true');
         gameDispatch({ type: 'update', subtype: 'cashMult' }); //sends building name to backend
         console.log('increment cash');
         gameDispatch({ type: 'update', subtype: 'setCash', cash: 500});
       }
-      if (buildingName === 'De La Guerra Dining Hall' && game?.cash >= 3000) {
+      if (buildingName === 'De La Guerra Dining Hall' && game?.cash >= 3000 && game?.knowledge >= 1500) {
         localStorage.setItem(buildingName, 'true');
         gameDispatch({ type: 'update', subtype: 'cashMult' }); //sends building name to backend
         console.log('increment cash');
         gameDispatch({ type: 'update', subtype: 'setCash', cash: 3000});
       }
-      if (buildingName === 'San Miguel Residence Hall' && game?.cash >= 25000) {
+      if (buildingName === 'San Miguel Residence Hall' && game?.cash >= 25000 && game?.knowledge >= 12500) {
         localStorage.setItem(buildingName, 'true');
         gameDispatch({ type: 'update', subtype: 'cashMult' }); //sends building name to backend
         console.log('increment cash');
         gameDispatch({ type: 'update', subtype: 'setCash', cash: 25000});
       }
-      if (buildingName === 'San Nicholas Residence Hall' && game?.cash >= 200000) {
+      if (buildingName === 'San Nicholas Residence Hall' && game?.cash >= 200000 && game?.knowledge >= 100000) {
         localStorage.setItem(buildingName, 'true');
         gameDispatch({ type: 'update', subtype: 'cashMult' }); //sends building name to backend
         console.log('increment cash');
         gameDispatch({ type: 'update', subtype: 'setCash', cash: 200000});
       }
-      if (buildingName === 'Santa Cruz Residence Hall' && game?.cash >= 1000000) {
+      if (buildingName === 'Santa Cruz Residence Hall' && game?.cash >= 1000000 && game?.knowledge >= 500000) {
         localStorage.setItem(buildingName, 'true');
         gameDispatch({ type: 'update', subtype: 'cashMult' }); //sends building name to backend
         console.log('increment cash');
         gameDispatch({ type: 'update', subtype: 'setCash', cash: 1000000});
       }
-      if (buildingName === 'Anacapa Residence Hall' && game?.cash >= 5000000) {
+      if (buildingName === 'Anacapa Residence Hall' && game?.cash >= 5000000 && game?.knowledge >= 2500000) {
         localStorage.setItem(buildingName, 'true');
         gameDispatch({ type: 'update', subtype: 'cashMult' }); //sends building name to backend
         console.log('increment cash');
         gameDispatch({ type: 'update', subtype: 'setCash', cash: 5000000});
       }
-      if (buildingName === 'Carrillo Dining Hall' && game?.cash >= 15000000) {
+      if (buildingName === 'Carrillo Dining Hall' && game?.cash >= 15000000  && game?.knowledge >= 7500000) {
         localStorage.setItem(buildingName, 'true');
         gameDispatch({ type: 'update', subtype: 'cashMult' }); //sends building name to backend
         console.log('increment cash');
         gameDispatch({ type: 'update', subtype: 'setCash', cash: 15000000});
       }
-      if (buildingName === 'Ortega Dining Hall' && game?.cash >= 500000000) {
+      if (buildingName === 'Ortega Dining Hall' && game?.cash >= 500000000 && game?.knowledge >= 250000000) {
         localStorage.setItem(buildingName, 'true');
         gameDispatch({ type: 'update', subtype: 'cashMult' }); //sends building name to backend
         console.log('increment cash');
         gameDispatch({ type: 'update', subtype: 'setCash', cash: 500000000});
       }
       //knowledgeBuildings
-      if (buildingName === 'Library' && game?.knowledge >= 100) {
+      if (buildingName === 'Library' && game?.knowledge >= 100 && game?.cash >=100) {
         localStorage.setItem(buildingName, 'true');
         gameDispatch({ type: 'update', subtype: 'knowledgeIncrementAmount', incrementKnowledgeAmount:2 }); //sends building name to backend
         console.log('increment knowledge');
         gameDispatch({ type: 'update', subtype: 'setCash', cash: 100});
       }
-      if (buildingName === 'Student Resource Building(SRB)' && game?.knowledge >= 500) {
+      if (buildingName === 'Student Resource Building(SRB)' && game?.knowledge >= 500 && game?.cash >=500) {
         localStorage.setItem(buildingName, 'true');
         gameDispatch({ type: 'update', subtype: 'knowledgeIncrementAmount', incrementKnowledgeAmount:5 }); //sends building name to backend
         console.log('increment knowledge');
         gameDispatch({ type: 'update', subtype: 'setCash', cash: 500});
       }
-      if (buildingName === 'Phelps Hall' && game?.knowledge >= 3000) {
+      if (buildingName === 'Phelps Hall' && game?.knowledge >= 3000 && game?.cash >=3000) {
         localStorage.setItem(buildingName, 'true');
         gameDispatch({ type: 'update', subtype: 'knowledgeIncrementAmount', incrementKnowledgeAmount:10 }); //sends building name to backend
         console.log('increment knowledge');
         gameDispatch({ type: 'update', subtype: 'setCash', cash: 3000});
       }
-      if (buildingName === 'Campbell Hall' && game?.knowledge >= 25000) {
+      if (buildingName === 'Campbell Hall' && game?.knowledge >= 25000 && game?.cash >=25000) {
         localStorage.setItem(buildingName, 'true');
         gameDispatch({ type: 'update', subtype: 'knowledgeIncrementAmount', incrementKnowledgeAmount:30 }); //sends building name to backend
         console.log('increment knowledge');
         gameDispatch({ type: 'update', subtype: 'setCash', cash: 25000});
       }
-      if (buildingName === 'Harold Frank Hall' && game?.knowledge >= 200000) {
+      if (buildingName === 'Harold Frank Hall' && game?.knowledge >= 200000 && game?.cash >=200000) {
         localStorage.setItem(buildingName, 'true');
         gameDispatch({ type: 'update', subtype: 'knowledgeIncrementAmount', incrementKnowledgeAmount:100 }); //sends building name to backend
         console.log('increment knowledge');
         gameDispatch({ type: 'update', subtype: 'setCash', cash: 200000});
       }
-      if (buildingName === 'Buchanan Hall' && game?.knowledge >= 1000000) {
+      if (buildingName === 'Buchanan Hall' && game?.knowledge >= 1000000 && game?.cash >=1000000) {
         localStorage.setItem(buildingName, 'true');
         gameDispatch({ type: 'update', subtype: 'knowledgeIncrementAmount', incrementKnowledgeAmount:500 }); //sends building name to backend
         console.log('increment knowledge');
         gameDispatch({ type: 'update', subtype: 'setCash', cash: 1000000});
       }
-      if (buildingName === 'Chemistry Building' && game?.knowledge >= 5000000) {
+      if (buildingName === 'Chemistry Building' && game?.knowledge >= 5000000 && game?.cash >=5000000) {
         localStorage.setItem(buildingName, 'true');
         gameDispatch({ type: 'update', subtype: 'knowledgeIncrementAmount', incrementKnowledgeAmount:1000 }); //sends building name to backend
         console.log('increment knowledge');
         gameDispatch({ type: 'update', subtype: 'setCash', cash: 5000000});
       }
-      if (buildingName === 'Broida Hall' && game?.knowledge >= 15000000) {
+      if (buildingName === 'Broida Hall' && game?.knowledge >= 15000000 && game?.cash >=15000000) {
         localStorage.setItem(buildingName, 'true');
         gameDispatch({ type: 'update', subtype: 'knowledgeIncrementAmount', incrementKnowledgeAmount:5000 }); //sends building name to backend
         console.log('increment knowledge');
@@ -201,6 +203,10 @@ const MonetaryBuildings: React.FC = () => {
               onClick={handleClick(building.name)}
               disabled={building.hasBeenBought} // Disable button if building has been bought
             >
+              COST: {building.cost}
+              <p> </p>
+              KNOWLEDGE REQUIRED: {building.knowledge}
+              <p> </p>
               Name: {building.name}
               <p> </p>
               Description: {building.description}
