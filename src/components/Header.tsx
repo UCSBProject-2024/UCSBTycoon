032c16dd2db4d2be76621a703fb0dc3ad6fcd17a
ZachDuckering <zedduckman@gmail.com>
2024-05-08 01:13:28 -0700

    Added a rankings drawer with an updating leaderboard

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,11 +13,5 @@ export function Header() {
         <h2>Chris Woolson, Aavash Adhikari, Tyler Canepa, Zach Duckering </h2>
       </div>
     </header>
-    
-    /*<header className="bg-blue-900 py-2">
-      <div className="text-right text-xs text-yellow-400">Chris Woolson, Chris Woolson,
-       Chris Woolson, Chris Woolson</div>
-      <div className="text-center text-xl uppercase text-yellow-400 md:text-3xl">UCSB Tycoon</div>
-    </header>*/
   );
 }
